c7b8e7b02a67015cfb23e538ba1dad0c30792b26
Dominic Desimini <ddesimini@gmail.com>
2020-12-02 15:33:00 -0700

    moved to c stuff

--- pwd.go
+++ pwd.go
@@ -1,126 +1,62 @@
 package gopasswd
 
-import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"regexp"
-	"strconv"
-	"strings"
-)
+/* #cgo CFLAGS: -g -Wall
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <pwd.h>
+ FILE * getpwdFile(){
+	FILE *fp;
+	fp = fopen("/etc/passwd", "w+");
+	return fp;
+ }
+
+*/
+import "C"
+import "unsafe"
 
 // Passwd is of same type as pwd from the C type
 type Passwd struct {
 	pwName   string
 	pwPasswd string
-	pwUID    int
-	pwGid    int
+	pwUID    uint32
+	pwGid    uint32
 	pwGecos  string
 	pwDir    string
 	pwShell  string
 }
 
-// GetPWUid returns user from /etc/passwd via uid
-func GetPWUid(uid int) (*Passwd, error) {
-
-	file, err := os.Open("/etc/passwd")
-	if err != nil {
-		panic(err)
+// Getpwnam Returns User name using glibc user functions
+func Getpwnam(name string) *Passwd {
+	cname := C.CString(name)
+	defer C.free(unsafe.Pointer(cname))
+	cpw := C.getpwnam(cname)
+	return &Passwd{
+		pwUID:   uint32(cpw.pw_uid),
+		pwGid:   uint32(cpw.pw_uid),
+		pwDir:   C.GoString(cpw.pw_dir),
+		pwShell: C.GoString(cpw.pw_shell),
 	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-
-	for scanner.Scan() {
-		user := strings.Split(scanner.Text(), ":")
-		userUID, _ := strconv.Atoi(user[2])
-		if userUID == uid {
-
-			userGid, _ := strconv.Atoi(user[3])
-			return &Passwd{
-				pwName:   user[0],
-				pwPasswd: user[1],
-				pwUID:    userUID,
-				pwGid:    userGid,
-				pwGecos:  user[4],
-				pwDir:    user[5],
-				pwShell:  user[6],
-			}, nil
-		}
-	}
-	return &Passwd{}, fmt.Errorf("Unable to Find UID")
 }
 
-func PutPWEnt(newUser *Passwd) (bool, error) {
-
-	userNameValidation, err := regexp.MatchString("[a-z_][a-z0-9_-]*[$]?", newUser.pwName)
-	if err != nil {
-		panic(err)
-	}
-
-	if userNameValidation {
-
-		uuid, err := getLastUserUid()
-		if err != nil {
-			return false, err
-		}
-
-		if uuid < 1000 {
-			file, err := getPWFile() 
-			if err != nil {
-				return false, err
-			}
-
-			passwdEntry := newUser.pwName + ":" + "x" + ":" + "1000" + ":" + "1000" + ":" + ":" + "/home/" + newUser.pwName + ":" + newUser.pwShell
-
-			file.WriteString(passwdEntry)
-			return true, nil
-		} else {
-			file, err := getPWDFile()
-			if err != nil {
-				panic(err)
-			}
-
-			passwdEntry := newUser.pwName + ":" + "x" + ":" + (getLastUserUid + 1) + ":" + (getLastUserUid + 1) ":" "/home/" + newUser.pwName + ":" + newUser.PwShell
-
-			file.WriteString(passwdEntry)
-
-			return true, nil
-		}
-	}
-
-	return false, fmt.Errorf("Unable to create user")
+// Getpwuid returns user from /etc/passwd via uid
+func Getpwuid(uid uint32) (*Passwd, error) {
+	cuid := C.uint(uid)
+	cpw := C.getpwuid(cuid)
+	return &Passwd{
+		pwUID:   uint32(cpw.pw_uid),
+		pwName:  C.GoString(cpw.pw_name),
+		pwDir:   C.GoString(cpw.pw_dir),
+		pwShell: C.GoString(cpw.pw_shell),
+		pwGecos: C.GoString(cpw.pw_gecos),
+	}, nil
 }
 
-func getPWDFile() (*os.File, error) {
-	file, err := os.OpenFile("/home/dom/passwd", os.O_APPEND|os.O_WRONLY, 0600)
-	if err != nil {
-		return file, err
+// Putpwent inserts user into /etc/passwd
+func Putpwent(user Passwd) int {
+	cPasswd := C.struct_passwd{
+		pw_uid: C.uint(user.pwUID),
 	}
-	defer file.Close()
+	cpw := C.putpwent(&cPasswd, C.getpwdFile())
 
-	return file, nil
-}
-
-func getLastUserUid() (int, error) {
-
-	file, err := os.Open("/etc/passwd")
-	if err != nil {
-		return 0, err
-	}
-	defer file.Close()
-	scanner := bufio.NewScanner(file)
-	var last []byte
-	for scanner.Scan() {
-		last = scanner.Bytes()
-	}
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
-
-	lastUser := strings.Split(string(last), ":")
-
-	return strconv.Atoi(lastUser[2])
-
+	return int(cpw)
 }
