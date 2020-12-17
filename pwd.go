143560954f95e4e74bb6fe3773459f743de9973c
Dominic Desimini <ddesimini@gmail.com>
2020-12-17 23:37:56 +0000

    made it through

--- pwd.go
+++ pwd.go
@@ -1,18 +1,13 @@
 package gopasswd
 
-/* #cgo CFLAGS: -g -Wall
- #include <stdio.h>
- #include <stdlib.h>
- #include <pwd.h>
- FILE * getpwdFile(){
-	FILE *fp;
-	fp = fopen("/etc/passwd", "w+");
-	return fp;
- }
-
-*/
-import "C"
-import "unsafe"
+import (
+	"bufio"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // Passwd is of same type as pwd from the C type
 type Passwd struct {
@@ -25,38 +20,139 @@ type Passwd struct {
 	pwShell  string
 }
 
-// Getpwnam Returns User name using glibc user functions
-func Getpwnam(name string) *Passwd {
-	cname := C.CString(name)
-	defer C.free(unsafe.Pointer(cname))
-	cpw := C.getpwnam(cname)
-	return &Passwd{
-		pwUID:   uint32(cpw.pw_uid),
-		pwGid:   uint32(cpw.pw_uid),
-		pwDir:   C.GoString(cpw.pw_dir),
-		pwShell: C.GoString(cpw.pw_shell),
+// GetPWUid returns user from /etc/passwd via uid
+func GetPWUid(uid int) (*Passwd, error) {
+
+	file, err := os.Open("/etc/passwd")
+	if err != nil {
+		return nil, err
 	}
 }
 
-// Getpwuid returns user from /etc/passwd via uid
-func Getpwuid(uid uint32) (*Passwd, error) {
-	cuid := C.uint(uid)
-	cpw := C.getpwuid(cuid)
-	return &Passwd{
-		pwUID:   uint32(cpw.pw_uid),
-		pwName:  C.GoString(cpw.pw_name),
-		pwDir:   C.GoString(cpw.pw_dir),
-		pwShell: C.GoString(cpw.pw_shell),
-		pwGecos: C.GoString(cpw.pw_gecos),
-	}, nil
+	for scanner.Scan() {
+		user := strings.Split(scanner.Text(), ":")
+		userUID, _ := strconv.Atoi(user[2])
+		if userUID == uid {
+			fmt.Println(user)
+			userGid, _ := strconv.Atoi(user[3])
+			return &Passwd{
+				pwName:   user[0],
+				pwPasswd: user[1],
+				pwUID:    userUID,
+				pwGid:    userGid,
+				pwGecos:  user[4],
+				pwDir:    user[5],
+				pwShell:  user[6],
+			}, nil
+		}
+	}
+	return &Passwd{}, fmt.Errorf("Unable to find UID")
 }
 
-// Putpwent inserts user into /etc/passwd
-func Putpwent(user Passwd) int {
-	cPasswd := C.struct_passwd{
-		pw_uid: C.uint(user.pwUID),
-	}
-	cpw := C.putpwent(&cPasswd, C.getpwdFile())
+//Getpwnam returns a user from /etc/passwd
+func Getpwnam(name string) (*Passwd, error) {
 
-	return int(cpw)
+	file, err := os.Open("/etc/passwd")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		user := strings.Split(scanner.Text(), ":")
+		userName := user[0]
+		if userName == name {
+			userUID, _ := strconv.Atoi(user[2])
+			userGid, _ := strconv.Atoi(user[3])
+			return &Passwd{
+				pwName:   user[0],
+				pwPasswd: user[1],
+				pwUID:    userUID,
+				pwGid:    userGid,
+				pwGecos:  user[4],
+				pwDir:    user[5],
+				pwShell:  user[6],
+			}, nil
+		}
+	}
+
+	return nil, fmt.Errorf("Unable to locate user with username")
+}
+
+// Putpwnam inserts a user struct into the /etc/passwrd file, if struct contains a password other then x
+// that password is placed in the /etc/shadow file
+func Putpwnam(user *Passwd) (bool, error) {
+	file, err := os.Open("/etc/passwd")
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		userEntry := strings.Split(scanner.Text(), ":")
+		userName := userEntry[0]
+		if userName == user.pwName {
+			return false, fmt.Errorf("User with username %s already exists", user.pwName)
+		}
+
+	}
+
+	fileWrite, err := os.OpenFile("/etc/passwd", os.O_APPEND|os.O_WRONLY, 0744)
+
+	fileWrite.WriteString(user.pwName + ":x:" + fmt.Sprint(user.pwUID) + ":" + fmt.Sprint(user.pwGid) + ":" + user.pwGecos + ":" + user.pwDir + ":" + user.pwShell)
+
+	return true, nil
+}
+
+//Rempwnam removes a user from /etc/passwd
+func Rempwnam(name string) (bool, error) {
+
+	file, err := os.Open("/etc/passwd")
+	if err != nil {
+		return false, err
+	}
+	defer file.Close()
+
+	tempFile, err := ioutil.TempFile("", "")
+	if err != nil {
+		return false, err
+	}
+	defer os.Remove(tempFile.Name())
+
+	scanner := bufio.NewScanner(file)
+
+	userExists := false
+	for scanner.Scan() {
+		userEntry := strings.Split(scanner.Text(), ":")
+		userName := userEntry[0]
+		if userName == name {
+			userExists = true
+			continue
+		}
+
+		if _, err := tempFile.WriteString(scanner.Text() + "\n"); err != nil {
+			return false, err
+		}
+	}
+
+	if err := file.Close(); err != nil {
+		return false, err
+	}
+
+	if err := tempFile.Close(); err != nil {
+		return false, err
+	}
+
+	if userExists == true {
+		if err := os.Rename(tempFile.Name(), "/etc/passwd"); err != nil {
+			return false, err
+
+		}
+	}
+
+	return true, nil
 }
--- pwd_test.go
+++ pwd_test.go
@@ -13,10 +13,62 @@ func TestGetPWUid(t *testing.T) {
 		t.Errorf("pwDir == %s; want /root", test.pwDir)
 	}
 
-	_, test2 := GetPWUid(93948)
-	errorValue := "Unable to Find UID"
-	if test2.Error() != errorValue {
-		t.Errorf("error == %v; Expected %s", test2, errorValue)
+	_, test2 := GetPWUid(93941)
+	if test2.Error() != "Unable to find UID" {
+		t.Errorf("pwName == %s; wanted error", test2.Error())
+	}
+}
+
+func TestGetpwnam(t *testing.T) {
+	testResult, _ := Getpwnam("root")
+
+	if testResult.pwName != "root" {
+		t.Errorf("pwName == %s; want root", testResult.pwName)
+	}
+
+	if testResult.pwDir != "/root" {
+		t.Errorf("Expected pwDir to be /root; Received: %s", testResult.pwDir)
+	}
+
+	_, testError := Getpwnam("Test_User_That_I_Hope_NEVER_Exists")
+
+	if testError.Error() != "Unable to locate user with username" {
+		t.Errorf("Expected error Message 'Unable to locate user with username; Received: %e", testError)
+	}
+}
+
+func TestPutpwname(t *testing.T) {
+	testUser := &Passwd{
+		pwName:   "TestUser123456",
+		pwPasswd: "x",
+		pwUID:    2001,
+		pwGid:    2001,
+		pwGecos:  "Test User May Delete",
+		pwDir:    "/home/TestUser123456",
+		pwShell:  "/bin/bash",
+	}
+	testSuccess, _ := Putpwnam(testUser)
+
+	if testSuccess != true {
+		t.Errorf("Expected True; Received: %t", testSuccess)
+	}
+
+	testFailure, _ := Putpwnam(testUser)
+
+	if testFailure != false {
+		t.Errorf("Expected false; Recieved: %t", testFailure)
+	}
+
+	testResult, _ := Getpwnam("TestUser123456")
+
+	if testResult.pwName != "TestUser123456" {
+		t.Errorf("Expected userName %s; Expected: 'TestUser123456'", testResult.pwName)
+	}
+
+	testCleanup, _ := Rempwnam("TestUser123456")
+
+	if testCleanup != true {
+		t.Errorf("Test Cleanup Failed")
 	}
 }
 
